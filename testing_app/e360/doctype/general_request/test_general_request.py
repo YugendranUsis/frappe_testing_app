477adbb5a297f3fb46d0d5c825d50321dd340fa9
Yugendran <yugendran@usistech.com>
2024-12-11 11:02:55 +0000

    testing app commited

---
+++ testing_app/e360/doctype/general_request/test_general_request.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, yugendran and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestGeneral_Request(FrappeTestCase):
+	pass
---
+++ testing_app/e360/doctype/leave_request/test_leave_request.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, yugendran and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestLeave_Request(FrappeTestCase):
+	pass
